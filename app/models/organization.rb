eba194cc76d55c405e72c8bf177baa21841425b1
KeithConroy <kconroyjr@gmail.com>
2020-09-21 12:45:49 -0700

    top volunteers

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -7,6 +7,17 @@ class Organization < ApplicationRecord
   has_many :users, through: :user_organizations
 
   def shifts
-    Shift.where(shift_type: [shift_types.pluck(:id)])
+    shift_types.flat_map(&:shifts)
   end
+
+  def top_volunteers
+    Shift.where(shift_type: [shift_types.pluck(:id)])
+      .joins(:user_shifts)
+      .group_by(&:users)
+      .map{|k,v| [k.first, v.count]}
+      .sort_by {|a| a[1]}
+      .first(10)
+      .to_h
+    end
+
 end
--- app/models/shift_type.rb
+++ app/models/shift_type.rb
@@ -5,6 +5,23 @@ class ShiftType < ApplicationRecord
   belongs_to :role, optional: true
   belongs_to :address
 
+  has_many :shifts
+
   scope :for_current_organization, -> { where(organization_id: Thread.current[:organization_id]) }
 
+  def fill_percentage
+    counts = shifts.completed.pluck(:remaining_spots, :spots)
+    remaining, total = counts.transpose.map(&:sum)
+    rate = (total - remaining) / total.to_f
+    "#{rate * 100}%"
+  end
+
+  def top_volunteers
+    shifts.joins(:user_shifts)
+      .group_by(&:users)
+      .map{|k,v| [k.first, v.count]}
+      .sort_by {|a| a[1]}
+      .first(10)
+      .to_h
+  end
 end
