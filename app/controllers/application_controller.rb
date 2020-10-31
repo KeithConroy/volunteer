0d0ddbbf5dfd8fd9fd42478e879e9a1927b75a09
KeithConroy <kconroyjr@gmail.com>
2020-10-31 11:16:18 -0700

    permit name for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_organization
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_organization
     if current_user
       @organization = current_user.admin_organization
       Thread.current[:organization_id] = current_user.admin_organization&.id
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
   end
 end
