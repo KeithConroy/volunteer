d22911ba31ace2856663fdb93e45756541557270
KeithConroy <kconroyjr@gmail.com>
2020-11-01 16:31:51 -0800

    rm facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :user_shifts, dependent: :destroy
   has_many :shifts, through: :user_shifts
